49740bc70f63148962e8cb31ee3345936eb5b0db
Ashish Verma <ashishrodio@gmail.com>
2017-02-28 00:06:37 +0530

    Added all validations - Pending Refactoring

--- js/calculator.js
+++ js/calculator.js
@@ -6,12 +6,20 @@ result.innerHTML = 0;
 
 var num = "";
 var isClicked = false;
+var counter = 0;
 var numbers = [];
 var operators = [];
 numberKey.map(function (val) {
     val.addEventListener('click', function (e) {
-        num = num.length <8 ? (num + (e.target.innerHTML)): num;
-        screen.innerHTML = (operators.length>0)?numbers[0]+operators[0]+num: num;
+        let number = e.target.innerHTML;
+        if(number == '.') {
+            counter++;
+        }
+        if(number== '.' && counter >1) {
+            return;
+        }
+        num = num.length < 8 ? (num + (number)) : num;
+        screen.innerHTML = (operators.length > 0) ? numbers[0] + operators[0] + num : num;
     })
 });
 
@@ -19,42 +27,56 @@ funcKey.map(function (val) {
 
     val.addEventListener('click', function (e) {
         let operator = e.target.innerHTML;
-        if(operator == "AC") {
+        counter = 0;
+        if (operator == "AC") {
             screen.innerHTML = "";
             result.innerHTML = 0;
             numbers.length = 0;
             operators.length = 0;
             num = "";
         } else if (operator == "CE") {
-            screen.innerHTML = result.innerHTML;
+            screen.innerHTML = "";
             operators.length = 0;
             numbers.length = 0;
             num = result.innerHTML;
         }
-        else if (num && operator != '=' && numbers.length < 2) {
+        else if (num && operator != '=' && numbers.length <= 2) {
             numbers.push(num);
             num = "";
             operators.push(operator);
-            console.log(operators);
-            console.log(numbers);
-            isClicked = false;
-            screen.innerHTML = numbers[0]+operators[0];
-        } else if (operator == '=' && !isClicked){
+            if (operators[1]) {
+                let finalResult = operate(numbers[0], numbers[1], operators[0]);
+                finalResult = isInt(finalResult) ? finalResult.toString() : finalResult.toFixed(2).toString();
+                result.innerHTML = finalResult.length > 10 ? "OUT OF RANGE" : finalResult;
+                num = result.innerHTML;
+                let newOperator = operators[1];
+                screen.innerHTML = num + operators[1];
+                numbers.length = 0;
+                operators.length = 0;
+                numbers[0] = num;
+                num = "";
+                operators[0] = newOperator;
+                isClicked = false;
+            } else {
+                isClicked = false;
+                screen.innerHTML = numbers[0] + operators[0];
+            }
+        } else if (operator == '=' && !isClicked) {
             numbers.push(num);
             let finalResult = operate(numbers[0], numbers[1], operators[0]);
-                screen.innerHTML = numbers[0]+operators[0]+numbers[1];
-                finalResult = isInt(finalResult)?finalResult.toString():finalResult.toFixed(2).toString();
-                result.innerHTML = finalResult.length>10?"OUT OF RANGE": finalResult;
-                numbers.length = 0;
-                num = parseFloat(result.innerHTML);
-                operators.length = 0;
-                isClicked = true;
+            screen.innerHTML = numbers[0] + operators[0] + numbers[1];
+            finalResult = isInt(finalResult) ? finalResult.toString() : finalResult.toFixed(2).toString();
+            result.innerHTML = finalResult.length > 10 ? "OUT OF RANGE" : finalResult;
+            numbers.length = 0;
+            num = parseFloat(result.innerHTML);
+            operators.length = 0;
+            isClicked = true;
         }
     })
 });
 
 function isInt(n) {
-   return n % 1 === 0;
+    return n % 1 === 0;
 }
 
 function operate(num1, num2, operator) {
